5536f3c63c30ff83587f4b46bbfc4543c84d2b23
yor_email <daniil.stebnitsky@gmail.com>
2020-08-21 16:21:40 +0300

    2. Чтение записей из телефонной книги.[188576#260337]

--- app/src/main/java/ru/job4j/readcontacts/MainActivity.java
+++ app/src/main/java/ru/job4j/readcontacts/MainActivity.java
@@ -2,29 +2,60 @@ package ru.job4j.readcontacts;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter<RecyclerView.ViewHolder> adapter;
+    private EditText et;
+    private String string = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
         RecyclerView recycler = findViewById(R.id.phones);
+        et = findViewById(R.id.editText);
+        string = et.getText().toString();
         recycler.setHasFixedSize(true);
         recycler.setLayoutManager(new LinearLayoutManager(this));
         List<String> phones = new ArrayList<>();
         adapter = new PhoneAdapter(phones);
         recycler.setAdapter(adapter);
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                string = et.getText().toString();
+                phones.clear();
+                loadDic(phones);
+            }
+        });
+        if (getApplicationContext().checkSelfPermission(Manifest.permission.READ_CONTACTS) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission
+                    .READ_CONTACTS}, 0);
+        }
         loadDic(phones);
     }
     private void loadDic(List<String> phones) {
@@ -32,8 +63,10 @@ public class MainActivity extends AppCompatActivity {
                 ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                 new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
                         ContactsContract.CommonDataKinds.Phone.NUMBER},
-                null,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
+                        + " like '%" + string + "%'",
                 null, null)) {
+            assert cursor != null;
             while (cursor.moveToNext()) {
                 String name = cursor.getString(cursor.getColumnIndex(ContactsContract
                         .CommonDataKinds.Phone.DISPLAY_NAME));
